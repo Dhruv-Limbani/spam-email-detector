bfb7b1384f03e328448c109cd4a857bf2256ed54
Dhruv <informdhruv@gmail.com>
2022-09-19 23:16:12 +0530

    main app in spam_detect

---
+++ spam_detect.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
+
+
+with open(r"D:\Users\DELL\Desktop\Streamlit webapps\Spam_Detection\email_classifier",'rb') as file:
+    v,clf = pickle.load(file)
+
+st.title("Email Spam Classifier")
+
+def predict(mail):
+    spam = clf.predict([mail])[0]
+    if spam:
+        st.error("It's a spam email")
+    else:
+        st.success("It's not a spam email")
+
+mail = st.text_area("Enter the mail")
+bt = st.button("Detect", on_click=predict(mail))
+
